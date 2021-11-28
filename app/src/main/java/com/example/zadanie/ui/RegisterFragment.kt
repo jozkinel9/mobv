0bca05f28f4f92b92935517aa8f6c0fef47d3da7
jozkinel9 <jozef.simek9@gmail.com>
2021-11-28 14:58:31 +0100

    login a register done

--- app/src/main/java/com/example/zadanie/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/zadanie/ui/RegisterFragment.kt
@@ -4,30 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.zadanie.R
+import com.example.zadanie.databinding.BasicFragmentBinding
+import com.example.zadanie.databinding.RegisterFragmentBinding
+import com.example.zadanie.ui.viewModels.BasicViewModel
 import com.example.zadanie.ui.viewModels.RegisterViewModel
 
 class RegisterFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RegisterFragment()
-    }
-
-    private lateinit var viewModel: RegisterViewModel
+    private val registerViewModel: RegisterViewModel by viewModels()
+    private lateinit var binding: RegisterFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.register_fragment, container, false)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+//      nav to HomeFragment
+        binding.registerBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+        }
+    }
 }
